b47d1dbd32144ae905a55c20ae5fc2538209d20c
ekeqiao <ekeqiao@163.com>
2018-08-27 21:38:49 +0800

    9.3.2

--- chapter9/NetworkTest2/app/src/main/java/com/example/administrator/networktest2/MainActivity.java
+++ chapter9/NetworkTest2/app/src/main/java/com/example/administrator/networktest2/MainActivity.java
@@ -3,18 +3,28 @@ package com.example.administrator.networktest2;
 import android.net.UrlQuerySanitizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.VideoView;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.ResponseCache;
 import java.net.URL;
+
+import javax.xml.parsers.SAXParserFactory;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -49,17 +59,87 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try{
                     OkHttpClient client = new OkHttpClient();
                     Request request= new Request.Builder()
-                            .url("http://www.paojiang.cn")
+                            //指定访问的服务器地址
+                            .url("http://www.paojiang.cn/get_data.xml")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseDatda=  response.body().string();
-                    showResponse(responseDatda);
-
+                   // showResponse(responseDatda);
+                    //parseXMLWithPull(responseDatda);
+                    parseXMlWithSAX(responseDatda);
                 }catch (Exception e){
                     e.printStackTrace();
                 }
             }
         }).start();
+    }
+    private void parseXMlWithSAX(String xmlData){
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            XMLReader xmlReader = factory.newSAXParser().getXMLReader();
+            ContentHandler handler = new ContentHandler();
+            //将
+            xmlReader.setContentHandler(handler);
+            xmlReader.parse(new InputSource(new StringReader(xmlData)));
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private void parseXMLWithPull(String xmlData){
+        try{
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser =factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int evenType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (evenType != xmlPullParser.END_DOCUMENT){
+                String nodeName = xmlPullParser.getName();
+                switch (evenType){
+                    //开始解析某个节点
+                    case  XmlPullParser.START_TAG:{
+                        if("id".equals(nodeName)){
+                            id = xmlPullParser.nextText();
+                        }else if("name".equals(nodeName)){
+                            name = xmlPullParser.nextText();
+                        }else if ("version".equals(nodeName)){
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+
+
+
+
+                    //完成解析某个节点
+                    case XmlPullParser.END_TAG:{
+                        if("app".equals(nodeName)){
+
+                            //responseText.setText(responseText.getText()+id+name+version);
+
+                            Log.d("MainActivity", "id is " + id);
+                            Log.d("MainActivity", "name is " + name);
+                            Log.d("MainActivity", "version is " + version);
+                           // Toast.makeText(MainActivity.this,"aaaaaa",Toast.LENGTH_SHORT).show();
+
+                        }
+                        break;
+                    }
+
+
+
+
+                    default:
+                        break;
+                }
+                evenType = xmlPullParser.next();
+            }
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     private void  sendRequestWithHttpURLConnection(){
         //开启线程来发起网络请求
@@ -105,7 +185,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void run() {
                 //在这里进行UI操作，将结果显示到界面上
-                responseText.setText(response);
+                responseText.setText(responseText.getText()+response);
             }
         });
     }
